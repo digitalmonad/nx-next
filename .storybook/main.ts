fbc4bf1947e841e0e59001ef14aa7be856e6029c
Pavel Kocman <kocmanpavel@gmail.com>
2024-07-17 13:07:50 +0200

    chore: Update Storybook to v8 (migration script)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,19 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../components/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: [
+    '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../modules/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  ],
   addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+
   framework: {
     name: '@storybook/nextjs',
     options: {},
+  },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
   },
 };
 
